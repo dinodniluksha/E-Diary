5d81605860d63a7239d080d5805780cabf874c60
Dinod Niluksha <dinodniluksha@gmail.com>
2021-10-11 01:11:05 +0530

    Added first trail node+express backend

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,75 @@
+const Note = require('../models/note.model.js');
+
+var fs = require('fs');
+var path = require('path');
+
+
+// Step 5 - set up multer for storing uploaded files
+ 
+var multer = require('multer');
+ 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+var upload = multer({ storage: storage });
+
+
+// Create and Save a new Note
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        //title: req.body.title, 
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    Note.find()
+    .then(notes => {
+        res.send(notes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    });
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
