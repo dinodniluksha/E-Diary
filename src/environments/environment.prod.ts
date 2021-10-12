59433895a509ab4152ca0268f032d3627c5455b3
Dinod Niluksha <dinodniluksha@gmail.com>
2021-10-12 13:53:03 +0530

    Completed setup firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD3R2-Ir7YiP7L12dt9YXMcJX0lGfPOJb0",
+    authDomain: "e-diary-1e542.firebaseapp.com",
+    projectId: "e-diary-1e542",
+    storageBucket: "e-diary-1e542.appspot.com",
+    messagingSenderId: "539858716005",
+    appId: "1:539858716005:web:226be82170bd3b568d8d8c",
+    measurementId: "G-5G8ZJK2C7P"
+  }
 };
