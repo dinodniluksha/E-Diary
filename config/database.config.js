71ad5327aec52c46904c1e5f7d123e85120e1af4
Dinod Niluksha <dinodniluksha@gmail.com>
2021-10-28 18:21:17 +0530

    Fixed the mistake

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    serverUrl: 'mongodb://localhost:27017/',
-    // serverUrl: process.env.MONGODB_URL
+    // serverUrl: 'mongodb://localhost:27017/',
+    serverUrl: process.env.MONGODB_URL
 }
